2e2ac1ef10002f3eff1f07b227628646a0e893b8
huanle0610 <huanle0610@gmail.com>
2018-08-02 22:46:48 +0800

    use bindNodeCallback

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ import {
   map, filter, switchMap, tap, skip, zip, finalize, buffer,
 } from 'rxjs/operators';
 import fetch from 'node-fetch';
+import fs from 'fs';
 
 
+import * as readline from 'readline';
 import readLine from './readline';
 import getFileContent from './file';
 import demoPow from './pow';
@@ -29,14 +31,43 @@ range(1, 20)
 
 console.log('more...');
 
-// getFileContent('./src/async.js')
+// demoPow();
 
-demoPow();
-
-// readLine()
-
-demoAsync();
+// demoAsync();
 
 from(fetch('https://jsonplaceholder.typicode.com/posts/1').then(r => r.json()))
   .pipe(map(v => `amtf - ${v.userId}`))
   .subscribe(console.log);
+
+const readdir$ = bindNodeCallback(fs.readdir);
+const source$ = readdir$('./src');
+// const subscription = source$.subscribe(
+//   res => console.log(`List of directories:  ${res} `),
+//   error => console.log(`Error:  ${error} `),
+//   () => console.log('Done!'),
+// );
+
+const readfile$ = bindNodeCallback(fs.readFile);
+const readSource$ = readfile$('/etc/hostsa', 'utf-8');
+// .subscribe(
+//   res => console.log(`file content ${res}`),
+// )
+
+readSource$.pipe(
+  zip(
+    source$,
+    (hosts, files) => {
+      console.log(hosts, files, 'haha');
+    },
+  ),
+).subscribe(
+  (x) => {
+    console.log(`Next: ${x}`);
+  },
+  (err) => {
+    console.log(`Error: ${err}`);
+  },
+  () => {
+    console.log('Completed');
+  },
+);
